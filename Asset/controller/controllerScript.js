0d40635fbf9f72785a76f0445b95d241fa77365a
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-04-30 18:24:36 +0530

    create the single page application

--- Asset/controller/controllerScript.js
+++ Asset/controller/controllerScript.js
@@ -4,3 +4,26 @@ $('.home-btn').on('click', function() {
     $('#items-content').css('display', 'none');
     $('#order-content').css('display', 'none');
 });
+
+
+$('.customer-btn').on('click', function() {
+    $('#home-content').css('display', 'none');
+    $('#customer-content').css('display', 'block');
+    $('#items-content').css('display', 'none');
+    $('#order-content').css('display', 'none');
+});
+
+
+$('.item-btn').on('click', function() {
+    $('#home-content').css('display', 'none');
+    $('#customer-content').css('display', 'none');
+    $('#items-content').css('display', 'block');
+    $('#order-content').css('display', 'none');
+});
+
+$('.orders-btn').on('click', function() {
+    $('#home-content').css('display', 'none');
+    $('#customer-content').css('display', 'none');
+    $('#items-content').css('display', 'none');
+    $('#order-content').css('display', 'block');
+});
