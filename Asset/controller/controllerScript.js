0f551f7653af978c8968c53899267824358b2463
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-04-30 18:23:24 +0530

    link the jquari lib

---
+++ Asset/controller/controllerScript.js
@@ -0,0 +1,6 @@
+$('.home-btn').on('click', function() {
+    $('#home-content').css('display', 'block');
+    $('#customer-content').css('display', 'none');
+    $('#items-content').css('display', 'none');
+    $('#order-content').css('display', 'none');
+});
