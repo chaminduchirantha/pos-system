e83c685b855fcc0320e8f2878d2574a8be8b10c4
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-05-02 09:19:22 +0530

    create the models in customer forms

---
+++ model/CustomerModel.js
@@ -0,0 +1,8 @@
+export default class CustomerModel {
+    constructor(fname,lname,address,salary) {
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+        this.salary = salary;
+    }
+}
