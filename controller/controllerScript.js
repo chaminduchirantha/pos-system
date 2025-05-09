b2c650c087650aab52d98e4282461025107dc8b9
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-05-09 07:42:06 +0530

    update the controllerScript file

--- controller/controllerScript.js
+++ controller/controllerScript.js
@@ -3,27 +3,42 @@ $('#home-button').on('click', function() {
     $('#customer-content').css('display', 'none');
     $('#items-content').css('display', 'none');
     $('#orders-content').css('display', 'none');
+    $('#order-detail-content').css('display', 'none');
 });
 
 
 $('#customer-nav').on('click', function() {
-    $('#home-container').css('display', 'none');
     $('#customer-content').css('display', 'block');
+    $('#home-container').css('display', 'none');
     $('#items-content').css('display', 'none');
     $('#orders-content').css('display', 'none');
+    $('#order-detail-content').css('display', 'none');
 });
 
 
 $('#item-nav').on('click', function() {
+    $('#items-content').css('display', 'block');
     $('#home-container').css('display', 'none');
     $('#customer-content').css('display', 'none');
-    $('#items-content').css('display', 'block');
     $('#orders-content').css('display', 'none');
+    $('#order-detail-content').css('display', 'none');
+
 });
 
 $('#order-nav').on('click', function() {
+    $('#orders-content').css('display', 'block');
     $('#home-container').css('display', 'none');
     $('#customer-content').css('display', 'none');
     $('#items-content').css('display', 'none');
-    $('#orders-content').css('display', 'block');
+    $('#order-detail-content').css('display', 'none');
+
+});
+
+$('#orderDetails-nav').on('click', function() {
+    $('#order-detail-content').css('display', 'block');
+    $('#home-container').css('display', 'none');
+    $('#customer-content').css('display', 'none');
+    $('#items-content').css('display', 'none');
+    $('#orders-content').css('display', 'none');
+
 });
